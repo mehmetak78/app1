b8403cf36437d53ec90037eaa4f5867bf80eb941
mehmetak <mehmetak78@hotmail.com>
2021-06-13 23:48:28 +0300

    Home

--- componentSamples/Features.js
+++ componentSamples/Features.js
@@ -37,7 +37,7 @@ const Features = (props) => {
         <div className="col-1-of-4">
           <div className="feature-box">
             <i className="feature-box__icon icon-basic-heart"></i>
-            <h3 className="heading-tertiary u-margin-bottom-small">Live a healthier life</h3>
+            <h3 className="heading-tertiary u-margin-bottom-small">Live healthier </h3>
             <p className="feature-box__text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, ipsum sapiente aspernatur.
             </p>
